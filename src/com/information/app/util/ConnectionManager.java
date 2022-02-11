93a5c70cd3dcb7b97f52a776f1269d016b093214
GCantet <gael.cantet@orange.fr>
2022-02-11 12:31:26 +0100

    ConnectionManager

--- src/com/information/app/util/ConnectionManager.java
+++ src/com/information/app/util/ConnectionManager.java
@@ -1,4 +1,27 @@
 package com.information.app.util;
 
+import java.sql.*;
+
 public class ConnectionManager {
+    static final String URL = "jdbc:mysql://localhost:3308/javaparadise?";
+    static final String USER = "root";
+    static final String PWD = "";
+    static Connection connection = null;
+
+    private static Connection getConnection() {
+        try {
+            connection = DriverManager.getConnection(URL, USER, PWD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private static void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
