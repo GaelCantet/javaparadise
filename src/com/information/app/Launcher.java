4486b519b1dd3e9c4cd4b6464d3287d15e65ca20
ArthurBDX <bideaux.arthur@gmail.com>
2022-02-11 15:54:39 +0100

    Launcher boucle

--- src/com/information/app/Launcher.java
+++ src/com/information/app/Launcher.java
@@ -8,40 +8,46 @@ import java.util.Scanner;
 
 public class Launcher {
     public static void main(String[] args) throws SQLException {
-        System.out.println("1 - Add a place");
-        System.out.println("2 - Find a place");
-        System.out.println("3 - Edit a place");
-        System.out.println("4 - Remove a place");
-        System.out.println("5 - Add a trip");
-        System.out.println("6 - Find a trip");
-        System.out.println("7 - Remove a trip");
-        System.out.println("8 - Quit");
 
-        Scanner scanner = new Scanner(System.in);
+        boolean start = true;
+        while(start){
+            System.out.println("1 - Add a place");
+            System.out.println("2 - Find a place");
+            System.out.println("3 - Edit a place");
+            System.out.println("4 - Remove a place");
+            System.out.println("5 - Add a trip");
+            System.out.println("6 - Find a trip");
+            System.out.println("7 - Remove a trip");
+            System.out.println("8 - Quit\n");
 
-        int choice = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
 
-        if(choice == 1){
-            System.out.print("Name : ");
-            String name = scanner.next();
-            Long id = new JdbcPlaceDao().createPlace(new Place(name));
-            System.out.println("id = "+id);
-        }else if(choice == 2){
-            // find place
-        }else if(choice == 3){
-            // edit place
-        }else if(choice == 4){
-            // remove place
-        }else if(choice == 5){
-            // add trip
-        }else if(choice == 6){
-            // find trip
-        }else if(choice == 7){
-            // remove trip
-        }else if(choice == 8){
-            // quit
-        }else{
-            System.out.println("erreur saisie");
+            int choice = scanner.nextInt();
+
+            if(choice == 1){
+                System.out.print("Name : ");
+                String name = scanner.next();
+                Long id = new JdbcPlaceDao().createPlace(new Place(name));
+                System.out.println("Place added with the ID = "+id+"\n");
+            }else if(choice == 2){
+                // find place
+            }else if(choice == 3){
+                // edit place
+            }else if(choice == 4){
+                // remove place
+            }else if(choice == 5){
+                // add trip
+            }else if(choice == 6){
+                // find trip
+            }else if(choice == 7){
+                // remove trip
+            }else if(choice == 8){
+                // quit
+                start = false;
+            }else{
+                System.out.println("erreur saisie");
+            }
         }
+
     }
 }
