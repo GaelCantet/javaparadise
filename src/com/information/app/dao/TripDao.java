e62e08a5e1e31375e18192b23104c88eb3883db2
ArthurBDX <bideaux.arthur@gmail.com>
2022-02-11 14:14:29 +0100

    TripDao

--- src/com/information/app/dao/TripDao.java
+++ src/com/information/app/dao/TripDao.java
@@ -1,4 +1,13 @@
 package com.information.app.dao;
 
+import com.information.app.model.Trip;
+
+import java.util.List;
+
 public interface TripDao {
+    public Long createTrip(Trip trip); //retournera un Long
+    public Trip findTripById(Long id); //retournera un Trip
+    public boolean updateTrip(Trip trip); //retourne un booléen
+    public boolean removeTrip(Trip trip); //retournera un booléen
+    public List<Trip> findAllTrip(); //retournera une liste de Trip
 }
