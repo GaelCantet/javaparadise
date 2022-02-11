1be2765b530701f9bdf1af19ade0702861e16c1f
GCantet <gael.cantet@orange.fr>
2022-02-11 11:40:37 +0100

    classes Place & Trip

--- src/com/information/app/model/Place.java
+++ src/com/information/app/model/Place.java
@@ -1,4 +1,22 @@
 package com.information.app.model;
 
 public class Place {
+    private long id;
+    private String name;
+
+    public Place(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
--- src/com/information/app/model/Trip.java
+++ src/com/information/app/model/Trip.java
@@ -1,4 +1,42 @@
 package com.information.app.model;
 
 public class Trip {
+    private long id;
+    private String departure;
+    private String arrival;
+    private float price;
+
+    public Trip(Place departure, Place arrival, float price) {
+        this.departure = departure.getName();
+        this.arrival = arrival.getName();
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setDeparture(Place departure) {
+        this.departure = departure.getName();
+    }
+
+    public void setArrival(Place arrival) {
+        this.arrival = arrival.getName();
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
