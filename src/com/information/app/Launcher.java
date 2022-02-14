ff5087e9be34336b558173ea4597ba2701d19e55
GCantet <gael.cantet@orange.fr>
2022-02-14 09:46:47 +0100

    interaction trip

--- src/com/information/app/Launcher.java
+++ src/com/information/app/Launcher.java
@@ -1,9 +1,12 @@
 package com.information.app;
 
 import com.information.app.dao.jdbc.JdbcPlaceDao;
+import com.information.app.dao.jdbc.JdbcTripDao;
 import com.information.app.model.Place;
+import com.information.app.model.Trip;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Launcher {
@@ -59,10 +62,40 @@ public class Launcher {
 
             }else if(choice == 5){
                 // add trip
+                System.out.println("Departure ID : ");
+                long departureId = scanner.nextLong();
+                Place departure = new JdbcPlaceDao().findPlaceById(departureId);
+                System.out.println("Departure : " + departure.getName());
+                System.out.println("Arrival ID : ");
+                long arrivalId = scanner.nextLong();
+                Place arrival = new JdbcPlaceDao().findPlaceById(arrivalId);
+                System.out.println("Arrival : " + arrival.getName());
+                System.out.println("Price : ");
+                float price = scanner.nextFloat();
+                long id = new JdbcTripDao().createTrip(new Trip(0, departure, arrival, price));
+                System.out.println("Trip added with id : "+id);
             }else if(choice == 6){
                 // find trip
+                System.out.println("Trip ID : ");
+                long tripId = scanner.nextLong();
+                Trip trip = new JdbcTripDao().findTripById(tripId);
+                System.out.println("Trip ID : " + trip.getId());
+                System.out.println("Trip departure : " + trip.getDeparture().getName());
+                System.out.println("Trip arrival : " + trip.getArrival().getName());
+                System.out.println("Trip price : " + trip.getPrice());
             }else if(choice == 7){
                 // remove trip
+                System.out.println("Trip ID : ");
+                long id = scanner.nextLong();
+                Trip trip = new JdbcTripDao().findTripById(id);
+                System.out.println(trip.getDeparture().getName() + " - " + trip.getArrival().getName() + ", " + trip.getPrice());
+                System.out.println("Are you sure tou want to delete this trip ? [yes/no]");
+                if (scanner.next().equals("yes")) {
+                    new JdbcTripDao().removeTrip(trip);
+                    System.out.println("Trip succesfully deleted");
+                } else {
+                    System.out.println("Deletion cancelled");
+                }
             }else if(choice == 8){
                 // quit
                 start = false;
